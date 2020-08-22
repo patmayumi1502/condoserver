940dcb59cd4ae3cdeb985791adaee39d9f4f233d
Darcio1974 <darcio1974@gmail.com>
2020-08-21 23:13:04 -0300

    microservice morador

---
+++ morador-service/src/main/java/com/condoserver/morador/moradorservice/MoradorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.condoserver.morador.moradorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoradorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoradorServiceApplication.class, args);
+	}
+
+}
---
+++ morador-service/src/test/java/com/condoserver/morador/moradorservice/MoradorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.condoserver.morador.moradorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoradorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

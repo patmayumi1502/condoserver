fba8262f8b9ff96dec165adfea43d16a6d137545
vilasboasgus <gustavovianavilasboas@gmail.com>
2020-08-26 00:43:43 -0300

    post morador e get morador

--- morador-service/src/main/java/com/condoserver/moradorservice/controller/MoradorController.java
+++ morador-service/src/main/java/com/condoserver/moradorservice/controller/MoradorController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.jackson.JsonObjectDeserializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,7 +28,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @Api(value = "morador")
-
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MoradorController {
 
 	@Autowired

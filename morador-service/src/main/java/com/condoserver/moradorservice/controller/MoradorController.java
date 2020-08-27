8463ddcc530206bb510258356f4f7728b7b21bc3
vilasboasgus <gustavovianavilasboas@gmail.com>
2020-08-26 21:14:53 -0300

    whats app, get moradores, post produto

--- morador-service/src/main/java/com/condoserver/moradorservice/controller/MoradorController.java
+++ morador-service/src/main/java/com/condoserver/moradorservice/controller/MoradorController.java
@@ -1,27 +1,15 @@
 package com.condoserver.moradorservice.controller;
 
-import java.net.URI;
-
-import org.apache.coyote.http11.Http11AprProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.jackson.JsonObjectDeserializer;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.condoserver.moradorservice.domain.Morador;
 import com.condoserver.moradorservice.repository.MoradorRepository;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -46,5 +34,11 @@ public class MoradorController {
 		
 		return moradorRepository.save(morador);
 	}	
+	
+	@ApiOperation(value = "Listagem de Moradores Cadastrados")
+	@GetMapping(value = "/moradores")
+	public Iterable<Morador> all() {
+		return moradorRepository.findAll();
+	}
 	
 }
